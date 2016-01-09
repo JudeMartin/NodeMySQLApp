f59c7e920e8249af18e21d230f069223eb79a4c9
Jude Martin <judemartin.mj@gmail.com>
2016-01-09 12:44:14 -0800

    Added the routes

---
+++ routes/customers.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jude on 1/9/16.
+ */
+
+exports.list = function (req, res) {
+
+}
+exports.add = function (req, res) {
+
+}
+exports.save = function (req, res) {
+
+}
+exports.delete_customer = function (req, res) {
+
+}
+exports.edit = function (req, res) {
+
+}
+exports.save_edit = function (req, res) {
+
+}
